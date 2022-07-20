3780aac8769f182b5ce1c77ba0699405ee06f365
gilangcsy <gilangchandra9@gmail.com>
2022-07-20 13:41:43 +0700

    change url traits

--- app/Http/Traits/UrlTrait.php
+++ app/Http/Traits/UrlTrait.php
@@ -3,6 +3,6 @@ namespace App\Http\Traits;
 
 trait UrlTrait {
     public static function url_dynamic() {
-        return request()->getClientIp() == '127.0.0.1' || request()->getClientIp() == 'localhost' ? 'http://localhost:3000/api/v1/' : '';
+        return request()->getClientIp() == '127.0.0.1' || request()->getClientIp() == 'localhost' ? 'http://localhost:3000/api/v1/' : 'https://api-selfdonalds.herokuapp.com/api/v1/';
     }
 }
